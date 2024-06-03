30569f7bcf8fe6f9ec7ecfba7041c519a5b83767
Rajesh <grajesh2906@gmail.com>
2024-06-03 22:43:22 +0530

    Added after making ht componet Reusable

--- render/src/App.jsx
+++ render/src/App.jsx
@@ -2,12 +2,76 @@ import List from './List.jsx'
 import './App.css'
 
 function App() {
-  
+  const fruits = [
+    { id: 1, name: "Apple", calories: "52" },
+    { id: 2, name: "Banana", calories: "96" },
+    { id: 3, name: "Cherry", calories: "530" },
+    { id: 4, name: "Date", calories: "282" },
+    { id: 5, name: "Elderberry", calories: "143" },
+    { id: 6, name: "Fig", calories: "74" },
+    { id: 7, name: "Grape", calories: "619" },
+    { id: 8, name: "Honeydew", calories: "36" },
+    { id: 9, name: "Indian Fig", calories: "41" },
+    { id: 10, name: "Jackfruit", calories: "95" }
+];
+const vegetables = [
+  { id: 1, name: "Carrot", calories: "41" },
+  { id: 2, name: "Broccoli", calories: "55" },
+  { id: 3, name: "Spinach", calories: "23" },
+  { id: 4, name: "Potato", calories: "77" },
+  { id: 5, name: "Tomato", calories: "18" },
+  { id: 6, name: "Cucumber", calories: "16" },
+  { id: 7, name: "Bell Pepper", calories: "20" },
+  { id: 8, name: "Onion", calories: "40" },
+  { id: 9, name: "Garlic", calories: "149" },
+  { id: 10, name: "Peas", calories: "81" }
+];
+const grains = [
+  { id: 1, name: "Rice", calories: "130" },
+  { id: 2, name: "Wheat", calories: "339" },
+  { id: 3, name: "Oats", calories: "389" },
+  { id: 4, name: "Barley", calories: "354" },
+  { id: 5, name: "Quinoa", calories: "120" },
+  { id: 6, name: "Corn", calories: "86" },
+  { id: 7, name: "Millet", calories: "119" },
+  { id: 8, name: "Rye", calories: "335" },
+  { id: 9, name: "Sorghum", calories: "329" },
+  { id: 10, name: "Buckwheat", calories: "343" }
+];
+const proteins = [
+  { id: 1, name: "Chicken", calories: "239" },
+  { id: 2, name: "Beef", calories: "250" },
+  { id: 3, name: "Pork", calories: "242" },
+  { id: 4, name: "Fish", calories: "206" },
+  { id: 5, name: "Eggs", calories: "155" },
+  { id: 6, name: "Tofu", calories: "76" },
+  { id: 7, name: "Lentils", calories: "116" },
+  { id: 8, name: "Chickpeas", calories: "364" },
+  { id: 9, name: "Beans", calories: "347" },
+  { id: 10, name: "Peas", calories: "81" }
+];
+const beverages = [
+  { id: 1, name: "Water", calories: "0" },
+  { id: 2, name: "Orange Juice", calories: "45" },
+  { id: 3, name: "Coffee", calories: "2" },
+  { id: 4, name: "Tea", calories: "2" },
+  { id: 5, name: "Soda", calories: "150" },
+  { id: 6, name: "Beer", calories: "43" },
+  { id: 7, name: "Wine", calories: "83" },
+  { id: 8, name: "Milk", calories: "42" },
+  { id: 9, name: "Smoothie", calories: "130" },
+  { id: 10, name: "Energy Drink", calories: "110" }
+];
+
  
 
   return (
     <>
-    <List />
+    {fruits.length > 0 ? <List items = {fruits} categories = "fruits"/> : null}
+    {/* //Short circuting*/}{vegetables.length > 0 && <List items = {vegetables} categories = "vegetables"/> }
+    <List items = {grains} categories = "grains"/>
+    <List items = {proteins} categories = "proteins"/>
+    <List items = {beverages} categories = "beverages"/>
     </>
   )
 }
--- render/src/List.jsx
+++ render/src/List.jsx
@@ -1,70 +1,95 @@
-// function List(){
-// const fruits = ["apple", "banana", "Orange", "mango"]
+// // function List(){
+// // const fruits = ["apple", "banana", "Orange", "mango"]
 
-// const listItems = fruits.map(fruit => <li>{fruit}</li> )
+// // const listItems = fruits.map(fruit => <li>{fruit}</li> )
 
-//     return(<ul>{listItems}</ul>);
-//     // return(<ol>{listItems}</ol>);
+// //     return(<ul>{listItems}</ul>);
+// //     // return(<ol>{listItems}</ol>);
+// // }
+
+
+//   // const listItems = fruits.map(fruit => <li key={fruit.id}>
+//     //                 {fruit.name} : &nbsp; <b>{fruit.calories}</b>
+//     // </li>);
+
+
+
+// function List(props){
+
+// //   //Displaying high calories friots only
+// //   const highCalories = fruits.filter(fruit => fruit.calories > 200);
+// //   const listItems = highCalories.map(highCalorie => 
+// //     <tr key={highCalorie.id}>
+// //         <td>{highCalorie.name}</td>
+// //         <td>{highCalorie.calories}</td>
+// //     </tr>
+// // );
+
+
+// //     // Displaying low calories fruits only
+// //     const lowCalories = fruits.filter(fruit => fruit.calories < 200);
+// //     const lowFruitData = lowCalories.map(lowCalorie => 
+// //         <tr key={lowCalorie.id}>
+// //             <td>{lowCalorie.name}</td>
+// //             <td>{lowCalorie.calories}</td>
+// //         </tr>
+// //     );
+
+
+//   //Displaying ALl data ie friots in the array
+//     // const listItems = fruits.map(fruit => 
+//     //     <tr key={fruit.id}>
+//     //         <td>{fruit.name}</td>
+//     //         <td>{fruit.calories}</td>
+//     //     </tr>
+//     // );
+
+
+//     return (
+//         <table border="1">
+//             <thead>
+//                 <tr>
+//                     <th>Fruit Name</th>
+//                     <th>Calories</th>
+//                 </tr>
+//             </thead>
+//             <tbody>
+//                 {listItems}
+//                 <tr>
+//                     <td>Low Calorie fruits</td>
+//                     <td>Caloreis</td>
+                   
+//                 </tr>
+//                 {lowFruitData}
+//             </tbody>
+//         </table>
+//     );
 // }
 
+//     // return (<ol>{listItems}</ol>)
 
-  // const listItems = fruits.map(fruit => <li key={fruit.id}>
-    //                 {fruit.name} : &nbsp; <b>{fruit.calories}</b>
-    // </li>);
+import PropTypes from 'prop-types';
+function List (props){
+    const category = props.categories;
+    const itemList = props.items;
 
+    const listItems = itemList.map(item => 
+        <li key={item.id}>
+        {item.name} : &nbsp;
+        {item.calories}
+        </li>
+    )
 
-
-function List(){
-
-  //Displaying high calories friots only
-  const highCalories = fruits.filter(fruit => fruit.calories > 200);
-  const listItems = highCalories.map(highCalorie => 
-    <tr key={highCalorie.id}>
-        <td>{highCalorie.name}</td>
-        <td>{highCalorie.calories}</td>
-    </tr>
-);
-
-
-    // Displaying low calories fruits only
-    const lowCalories = fruits.filter(fruit => fruit.calories < 200);
-    const lowFruitData = lowCalories.map(lowCalorie => 
-        <tr key={lowCalorie.id}>
-            <td>{lowCalorie.name}</td>
-            <td>{lowCalorie.calories}</td>
-        </tr>
-    );
-
-
-  //Displaying ALl data ie friots in the array
-    // const listItems = fruits.map(fruit => 
-    //     <tr key={fruit.id}>
-    //         <td>{fruit.name}</td>
-    //         <td>{fruit.calories}</td>
-    //     </tr>
-    // );
-    return (
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>Fruit Name</th>
-                    <th>Calories</th>
-                </tr>
-            </thead>
-            <tbody>
-                {listItems}
-                <tr>
-                    <td>Low Calorie fruits</td>
-                    <td>Caloreis</td>
-                   
-                </tr>
-                {lowFruitData}
-            </tbody>
-        </table>
+    return(
+        <>
+        <h3 className="Category">{category}</h3>
+        <ul className="List-item">{listItems}</ul>
+        </>
     );
 }
 
-    // return (<ol>{listItems}</ol>)
-
-
+List.defaultProps = {
+    category : "Category",
+    itemList : [],
+}
 export default List;
